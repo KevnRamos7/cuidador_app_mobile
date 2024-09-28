545d675b5812d551ced97d12842767647842e616
IDGS-803-21000435 <Simsonvh@outlook.com>
2024-09-27 20:01:10 -0600

    fix: request api refactorization

--- lib/Data/Request/Contrato/contrato_request.dart
+++ lib/Data/Request/Contrato/contrato_request.dart
@@ -1,5 +1,3 @@
-
-
 import 'dart:convert';
 
 import 'package:cuidador_app_mobile/Domain/Model/Contrato/contrato_model.dart';
@@ -8,60 +6,56 @@ import 'package:cuidador_app_mobile/UI/Shared/Snackbar/snackbar_ui.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 
-class ContratoRequest extends GetConnect{
-
+class ContratoRequest extends GetConnect {
   SnackbarUI snackbarUI = SnackbarUI();
 
-  Future<bool> saveContratoInDB(ContratoModel contrato) async{
-
-    String url = '${ConnectionString.connectionString}ContratoItem/guardarContrato';
+  Future<bool> saveContratoInDB(ContratoModel contrato) async {
+    String url =
+        '${ConnectionString.connectionString}ContratoItem/guardarContrato';
     Map<String, dynamic> jsonRequest = {
-      'persona_cuidador_id' : 6, //contrato.personaCuidador!.idPersona,
-      'persona_cliente_id' : 4, //contrato.personaCliente!.idPersona,
-      'contrato_item' : contrato.contratoItem!.map((e) => {
-        'observaciones' : e.observaciones,
-        'horario_inicio_propuesto' : e.horarioInicioPropuesto?.toIso8601String() ?? '',
-        'horario_fin_propuesto' : e.horarioFinPropuesto?.toIso8601String() ?? '',
-        'tareas_contrato' : e.tareasContrato!.map((i) => {
-          'tituloTarea' : i.tituloTarea,
-          'descripcionTarea' : i.descripcionTarea,
-          'tipoTarea' : i.tipoTarea,
-          'fechaARealizar' : i.fechaRealizar?.toIso8601String() ?? ''
-        }).toList()
-      }).toList()
+      'persona_cuidador_id': 6, //contrato.personaCuidador!.idPersona,
+      'persona_cliente_id': 4, //contrato.personaCliente!.idPersona,
+      'contrato_item': contrato.contratoItem!
+          .map((e) => {
+                'observaciones': e.observaciones,
+                'horario_inicio_propuesto':
+                    e.horarioInicioPropuesto?.toIso8601String() ?? '',
+                'horario_fin_propuesto':
+                    e.horarioFinPropuesto?.toIso8601String() ?? '',
+                'tareas_contrato': e.tareasContrato!
+                    .map((i) => {
+                          'tituloTarea': i.tituloTarea,
+                          'descripcionTarea': i.descripcionTarea,
+                          'tipoTarea': i.tipoTarea,
+                          'fechaARealizar':
+                              i.fechaRealizar?.toIso8601String() ?? ''
+                        })
+                    .toList()
+              })
+          .toList()
     };
 
-    print(jsonRequest);
+    try {
+      http.Response response = await http.post(Uri.parse(url),
+          headers: {'Content-Type': 'application/json'},
+          body: jsonEncode(jsonRequest));
 
-
-    try
-    {
-
-      http.Response response = await http.post(
-        Uri.parse(url),
-        headers: {'Content-Type': 'application/json'},
-        body: jsonEncode(jsonRequest));
-
-      if(response.statusCode == 200)
-      {
-        return true;
+      switch (response.statusCode) {
+        case 200:
+          return true;
+        case 400:
+          snackbarUI.snackbarError("formato peticion incorrecto", "");
+          return false;
+        case 404:
+          snackbarUI.snackbarError("No se encontro", "");
+          return false;
+        default:
+          snackbarUI.snackbarError("Se produjo un error desconocido", "");
+          return false;
       }
-      else
-      {
-        snackbarUI.snackbarError('Ha Ocurrido Un Error', 'Error al guardar el contrato');
-        return false;
-      }
-
-    }
-    catch(e)
-    {
-
+    } catch (e) {
       snackbarUI.snackbarError('Ha Ocurrido Un Error', 'Error al guardar el contrato');
-      print(e);
       return false;
-
     }
-
   }
-
 }
